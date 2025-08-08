ce48cf1a7e180fb99cf56cf074e1fed76c8240a1
yousraamr <yousraamr000@gmail.com>
2025-08-08 02:17:16 +0200

    Link local project to GitHub repo

---
+++ android/app/src/main/kotlin/com/example/isplit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.isplit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Generated file by firebase
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'iSplit',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('iSplit Home')),
+        body: const Center(child: Text('Firebase initialized!')),
+      ),
+    );
+  }
+}
